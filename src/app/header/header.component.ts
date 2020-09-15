d82397a189bd71d4788f8c5ed03f538f3e5c063f
eldhopaulose <eldhopaulose0485@gmail.com>
2020-09-15 23:35:17 +0530

    componets are modified v0.0.01

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,15 @@
-import  { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
-
 export class HeaderComponent implements OnInit {
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit(): void {
+  }
+
 }
